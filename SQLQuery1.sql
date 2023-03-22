e12686c8df6b72192366c98e074dc27f56d1f543
gnizamo <gokceb1903@gmail.com>
2023-03-22 16:27:39 +0300

    hangi üründen kaç adet satıldığı bilgisi azalan sıraya göre

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -1,6 +1,14 @@
 -- employees who can't make sales at all --
+--------------------------------------------
 SELECT * FROM Employees as e left join Orders as o 
 on e.EmployeeID = o.EmployeeID 
 where o.EmployeeID is null
--- no employee who can not make sales 
+----------------------------------------------------------
+-- information about how many units of which product have been sold -- 
+SELECT p.ProductName, COUNT(*) as Piece FROM Products as p inner join [Order Details] as od
+on p.ProductID = od.ProductID
+group by p.ProductName
+order by Piece desc
+
+
 
